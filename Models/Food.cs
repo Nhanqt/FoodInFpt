98fc5dc52e8a47b8d28854624c80d982320ab8d8
QuangTrongNhan <nhanqt2809@gmail.com>
2021-11-28 20:44:17 +0700

    update model

--- Models/Food.cs
+++ Models/Food.cs
@@ -9,6 +9,5 @@ namespace foodFptApi.Models
         public string Description { get; set; }
         public decimal price { get; set; }
         public string image { get; set; }
-        public decimal total { get; set; }
     }
 }
